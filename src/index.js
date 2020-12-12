705046aa73f83c20ff78c972fb588cb57e399397
MilaHavrysh <ludmilu2017@ukr.net>
2020-12-12 20:26:53 +0200

    создана разметка

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import './styles.css';
+import menuArray from './menu.json';
+import menuTemplate from './templates/menu-templates.hbs';
+
+//console.log(menuArray)
+console.log(menuTemplate)
+const create = menuTemplate(menuArray)
+console.log(create)
+//menuTemplate(menuArray)
+
+//const menu = createMenu(menuItemTemplate);
+const menu = document.querySelector('.menu')
+menu.insertAdjacentHTML('beforeend', create)
