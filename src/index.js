53ee67f5776747fa9f1fe8c2a794c938a3581730
MilaHavrysh <ludmilu2017@ukr.net>
2020-12-13 18:50:59 +0200

    Добавлена загрузка темы

--- src/index.js
+++ src/index.js
@@ -1,13 +1,46 @@
 import './styles.css';
 import menuArray from './menu.json';
 import menuTemplate from './templates/menu-templates.hbs';
-
-//console.log(menuArray)
-console.log(menuTemplate)
 const create = menuTemplate(menuArray)
-console.log(create)
-//menuTemplate(menuArray)
-
-//const menu = createMenu(menuItemTemplate);
 const menu = document.querySelector('.menu')
+const bodyTeg = document.querySelector('body');
+const input = document.querySelector('#theme-switch-toggle');
+const loadTheme = localStorage.getItem('theme')
+
+
 menu.insertAdjacentHTML('beforeend', create)
+
+//------localStorage------//
+input.addEventListener('change', changeTheme)
+
+function loadPageTheme() {
+  bodyTeg.classList.toggle(loadTheme)
+  if (loadTheme === 'dark-theme') {
+    input.checked=true
+}
+}
+
+loadPageTheme()
+
+function changeTheme(event) { 
+  let setTheme
+  let saveTheme
+  if (event.target.checked == false) {
+      setTheme = 'light-theme'; 
+   }
+    else{setTheme = 'dark-theme';  
+ }
+  bodyTeg.className = setTheme;
+  saveTheme=localStorage.setItem('theme',setTheme)  
+}
+
+
+
+
+
+
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
